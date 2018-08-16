bffab8b9b5c0315930c92e775d58d34b1c372448
Bryan Leung <bryanlng@cs.utexas.edu>
2018-08-15 22:37:10 -0500

    added basic method definition for risk. Need to 1)Implement Django REST framework, then 2) write tests for performance first before I implement these methods

--- RiskStats.py
+++ RiskStats.py
@@ -16,60 +16,69 @@ class RiskStats:
             8. Capture ratios (5 time periods: 1 year, 3 year, 5 year, 10 year, 15 year)
         Return in a JsonResponse encoded object
         """
-        print ("")
+        response = {}
+        response["alphas"] = self.get_alphas(fund_symbol)
+        response["betas"] = self.get_betas(fund_symbol)
+        response["rsquareds"] = self.get_r_squareds(fund_symbol)
+        response["stddevs"] = self.get_stddevs(fund_symbol)
+        response["sharpes"] = self.get_sharpe_ratios(fund_symbol)
+        response["sortinos"] = self.get_sortino_ratios(fund_symbol)
+        response["treynors"] = self.get_treynor_ratios(fund_symbol)
+        response["captures"] = self.get_capture_ratios(fund_symbol)
+        return response
 
 
     def get_alphas(fund_symbol):
         """
         Gets alpha values for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_betas(fund_symbol):
         """
         Gets beta values for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_r_squareds(fund_symbol):
         """
         Gets R-squared values for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_stddevs(fund_symbol):
         """
         Gets standard deviation values for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_sharpe_ratios(fund_symbol):
         """
         Gets sharpe ratios for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_sortino_ratios(fund_symbol):
         """
         Gets sortino ratios for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_treynor_ratios(fund_symbol):
         """
         Gets treynor ratios for 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
 
 
     def get_capture_ratios(fund_symbol):
         """
         Gets upside and downside capture ratios for 1 year, 3 year, 5 year, 10 year, 15 year
         """
-        print ("")
+        return {}
