9279ed873ea9d55d96e1f006bd467623b523894c
Bryan Leung <bleungtx2@gmail.com>
2018-09-06 16:59:54 -0600

    reorganized methods, condensed methods from 10 --> 4

--- fundtool/fundapi/libraries/General.py
+++ fundtool/fundapi/libraries/General.py
@@ -20,57 +20,33 @@ class GeneralStats:
             8. Morningstar category
             9. Turnover ratio
         Source = Morningstar, quotes page
-
-
-        Groups in terms of GET requests:
-        1. header request:
-            response["price"] = self.get_price(fund_symbol)
-            response["min_investment"] = self.get_min_investment(fund_symbol)
-            response["expense_ratio"] = self.get_expense_ratio(fund_symbol)
-            response["turnover_ratio"] = self.get_turnover_ratio(fund_symbol)
-            response["morningstar_category"] = self.get_morningstar_category(fund_symbol)
-
-        2. asset allocation
-            response["asset_allocation"] = self.get_asset_allocation_data(fund_symbol)
-
-        3. risk measures
-            response["morningstar_risk_vs_category"] = self.get_morningstar_risk_vs_category(fund_symbol)
-            response["morningstar_return_vs_category"] = self.get_morningstar_return_vs_category(fund_symbol)
-
-        4. security identifier get request
-            response["morningstar_overall_rating"] = self.get_morningstar_overall_rating(fund_symbol)
         """
 
         response = {}
 
+        sections = [Section.GENERAL_STATS, Section.ASSET_ALLOCATION, Section.RISK_RETURN_VS_CATEGORY, Section.OVERALL_RATING]
+        # for section in sections:
 
-        response["price"] = self.get_price(fund_symbol)
-        response["min_investment"] = self.get_min_investment(fund_symbol)
-        response["expense_ratio"] = self.get_expense_ratio(fund_symbol)
-        response["asset_allocation"] = self.get_asset_allocation_data(fund_symbol)
-        response["morningstar_overall_rating"] = self.get_morningstar_overall_rating(fund_symbol)
-        response["morningstar_risk_vs_category"] = self.get_morningstar_risk_vs_category(fund_symbol)
-        response["morningstar_return_vs_category"] = self.get_morningstar_return_vs_category(fund_symbol)
-        response["morningstar_category"] = self.get_morningstar_category(fund_symbol)
-        response["turnover_ratio"] = self.get_turnover_ratio(fund_symbol)
+        # response["price"] = self.get_price(fund_symbol)
+        # response["min_investment"] = self.get_min_investment(fund_symbol)
+        # response["expense_ratio"] = self.get_expense_ratio(fund_symbol)
+        # response["asset_allocation"] = self.get_asset_allocation_data(fund_symbol)
+        # response["morningstar_overall_rating"] = self.get_morningstar_overall_rating(fund_symbol)
+        # response["morningstar_risk_vs_category"] = self.get_morningstar_risk_vs_category(fund_symbol)
+        # response["morningstar_return_vs_category"] = self.get_morningstar_return_vs_category(fund_symbol)
+        # response["morningstar_category"] = self.get_morningstar_category(fund_symbol)
+        # response["turnover_ratio"] = self.get_turnover_ratio(fund_symbol)
         return {}
 
 
-    def get_price(self, fund_symbol):
+    def get_general_details(self, fund_symbol):
         """
-        Gets most up-to-date NAV of the mutual fund
-        """
-        return {}
-
-    def get_min_investment(self, fund_symbol):
-        """
-        Gets minimum amount of $$ required to invest in the mutual fund
-        """
-        return {}
-
-    def get_expense_ratio(self, fund_symbol):
-        """
-        Gets expense ratio (in percentage, ex: .77%)
+        Gets the following:
+            1. Price/ NAV
+            2. Minimum investment
+            3. Expense ratio (in percentage, ex: .77%)
+            4. Turnover ratio (in percentage, ex: .77%)
+            5. Morningstar Category
         """
         return {}
 
@@ -81,34 +57,17 @@ class GeneralStats:
         """
         return {}
 
+    def get_risk_return_vs_category(self, fund_symbol):
+        """
+        Gets the:
+            1. overall risk compared to its category, as judged by Morningstar
+            2. overall return compared to its category, as judged by Morningstar
+        Found on quotes page
+        """
+        return {}
+
     def get_morningstar_overall_rating(self, fund_symbol):
         """
         Gets the overall Morningstar rating
-        """
-        return {}
-
-    def get_morningstar_risk_vs_category(self, fund_symbol):
-        """
-        Gets the overall risk compared to its category, as judged by Morningstar
-        Found on quotes page
-        """
-        return {}
-
-    def get_morningstar_return_vs_category(self, fund_symbol):
-        """
-        Gets the overall return compared to its category, as judged by Morningstar
-        Found on quotes page
-        """
-        return {}
-
-    def get_morningstar_category(self, fund_symbol):
-        """
-        Gets the type of category/sector the fund is in
-        """
-        return {}
-
-    def get_turnover_ratio(self, fund_symbol):
-        """
-        Gets turnover ratio of the mutual fund
         """
         return {}
