fd49cb34983b4e4f467432b5e2f8bdff6357c82c
Bryan Leung <bleungtx2@gmail.com>
2018-09-06 00:20:37 -0600

    Refactored the SHIT out of Risk, since I realized that all 3 of my methods (mpt, volatility, capture ratios) code were pretty much the same, with just some variation during the part where we extract the td tags. Ran into a problem where it didn't exactly mesh, b/c capture ratios only requires 1 GET request to get all years, but MPT and Volatility requires 4 GET requests each for each year, since each year is a different # in the year parameter, which means a different url. Also ran into an issue with combining 2 dicts into one, but I found a solution for that

--- misc/Risk_no_refactoring.py
+++ misc/Risk_no_refactoring.py
@@ -20,32 +20,24 @@ class RiskStats:
             8. Capture ratios (5 time periods: 1 year, 3 year, 5 year, 10 year, 15 year)
         Return in a JsonResponse encoded object
         """
-
-        #Add data from capture ratios first. We can get all data in capture ratios in 1 get request, but need multiple for mpt and volatility
-        response = self.get_capture_ratios(fund_symbol)
-
-        timespans = ["3-Year", "5-Year", "10-Year", "15-Year"]
-        for timespan in timespans:
-            #Extract and aggregate data for MPT stats and Volatility stats
-            mpt_and_volatility = self.collect_column_data(fund_symbol, timespan)
-
-            #Add these values into the current timespan dict along with the capture ratios
-            response[timespan] = {**response[timespan], **mpt_and_volatility}
-
+        response = {}
+        response["mpt_stats"] = self.get_mpt_stats(fund_symbol)
+        response["volatility_stats"] = self.get_volatility_stats(fund_symbol)
+        response["capture_ratios"] = self.get_capture_ratios(fund_symbol)
         return response
 
-
-    def collect_column_data(self,fund_symbol, timespan):
+    def get_mpt_stats(self, fund_symbol, timespan):
         """
-        For a given timespan, gets ALL the MPT + Volatility data
+        Retrieves alpha, beta, R-squared, Treynor ratio
         """
-        timespan_dict = {}
-        year = timespan.split("-")[0]
-        sections = [Section.RISK_MPT, Section.RISK_VOLATILITY]
+        # Build a dictionary, where key = time period, value = trailing return for that time period.
+        timespans = ["3-Year", "5-Year", "10-Year", "15-Year"]
+        fields = ["Category Index", "R-Squared", "Beta", "Alpha", "Treynor Ratio", "Currency"]
+        response = {}
 
-        for section in sections:
-            section_dict = {}
-            url = Util.build_url(section, fund_symbol, year)
+        for timespan in timespans:
+            year = timespan.split("-")[0]
+            url = Util.build_url(Section.RISK_MPT, fund_symbol, year)
             raw = requests.get(url)
             if raw.status_code == 200 and raw.text != "":
                 print("200 and not empty")
@@ -60,36 +52,56 @@ class RiskStats:
                         row_header = row.find("th")
                         if dataNotFoundYet and row_header != None and row_header.text == fund_symbol:
                             dataNotFoundYet = False
-                            section_dict = self.extract_column_data(row, section)
-
-                            #Accumulate key-value pairs of section_dict into timespan_dict
-                            timespan_dict = {**timespan_dict, **section_dict}
+                            stats = [col.text.strip() for col in row.findAll("td")]
+                            response[timespan] = dict(zip(fields, stats))
                 else:
                     raise FundException.UIChangedError(f"Error while retrieving data for risk mpt: UI for source website of this symbol has changed, so we can't scrape the data: {fund_symbol}")
             else:
                 raise FundException.SymbolDoesNotExistError(f"Error while retrieving data for risk mpt: Symbol does not exist: {fund_symbol}")
 
-        return timespan_dict
-
-    def extract_column_data(self, row, section):
-        response = {}
-        if section == Section.RISK_MPT:
-            fields = ["Category Index", "R-Squared", "Beta", "Alpha", "Treynor Ratio", "Currency"]
-            stats = [col.text.strip() for col in row.findAll("td")]
-            response = dict(zip(fields, stats))
-        else:
-            fields = ["Standard Deviation", "Return", "Sharpe Ratio", "Sortino Ratio"]
-            stats = [col.text.strip() for col in row.findAll("td")]
-            del stats[len(stats)-1]                             #Remove unnecessary values
-            response = dict(zip(fields, stats))
         return response
 
-    def get_capture_ratios(self, fund_symbol):
+    def get_volatility_stats(self, fund_symbol, timespan):
+        """
+        Retrieves standard deviation, return, sharpe ratio, sortino ratio
+        """
+        # Build a dictionary, where key = time period, value = trailing return for that time period.
+        timespans = ["3-Year", "5-Year", "10-Year", "15-Year"]
+        fields = ["Standard Deviation", "Return", "Sharpe Ratio", "Sortino Ratio"]
+        response = {}
+
+        for timespan in timespans:
+            year = timespan.split("-")[0]
+            url = Util.build_url(Section.RISK_VOLATILITY, fund_symbol, year)
+            raw = requests.get(url)
+            if raw.status_code == 200 and raw.text != "":
+                print("200 and not empty")
+                soup = BeautifulSoup(raw.text, 'html.parser')
+
+                # Find corresponding column values of trailing risk stats. These will be the values of the dict
+                table = soup.find("table")
+                if table is not None:
+                    rows = table.findAll(lambda tag: tag.name == 'tr')
+                    for row in rows:
+                        row_header = row.find("th")
+                        if row_header != None and row_header.text == fund_symbol:
+                            stats = [col.text.strip() for col in row.findAll("td")]
+                            del stats[len(stats)-1]                             #Remove unnecessary values
+                            response[timespan] = dict(zip(fields, stats))
+                else:
+                    raise FundException.UIChangedError(f"Error while retrieving data for risk volatility statistics: UI for source website of this symbol has changed, so we can't scrape the data: {fund_symbol}")
+            else:
+                raise FundException.SymbolDoesNotExistError(f"Error while retrieving data for risk volatility statistics: Symbol does not exist: {fund_symbol}")
+
+        return response
+
+    def get_capture_ratios(self, fund_symbol, timespan):
         """
         Gets upside and downside capture ratios for 1 year, 3 year, 5 year, 10 year, 15 year
         """
         # Build a dictionary, where key = time period, value = trailing return for that time period.
         timespans = ["3-Year", "5-Year", "10-Year", "15-Year"]
+        upsidedownside_fields = ["Upside ratio", "Downside ratio"]
         fields = ["Standard Deviation", "Return", "Sharpe Ratio", "Sortino Ratio"]
         response = {}
 
@@ -113,7 +125,7 @@ class RiskStats:
                             first_dot = raw.find(".")
                             upside_ratio = raw[:first_dot+3]
                             downside_ratio = raw[first_dot+3:]
-                            stats.append({"Upside Ratio": upside_ratio, "Downside ratio": downside_ratio})
+                            stats.append({"upside_ratio": upside_ratio, "downside_ratio": downside_ratio})
 
                         del stats[0]       #Delete 1-Year for consistency, since other stats only have 3year, 5year, 10year, 15year
                         response = dict(zip(timespans, stats))
